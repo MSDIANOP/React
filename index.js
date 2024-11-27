db5dbd3eae96d12e73e3bb3b47ed502f7f714cda
MSDIANOP <chiragchhugani20@gmail.com>
2024-11-27 16:48:18 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-function App(){
-    return <p>Hello,World!</p>
+const {useState}=React;
+const App=()=>{
+const [Title, setTitle] = useState("React Title");
+  const [Updates, setUpdates] = useState(0);
+  const handleChange = () => {
+    setTitle("Updated React Title");
+    setUpdates((prev) => prev + 1);
+  };
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>{Title}</h1>
+      <button onClick={handleChange}>Change Title (React)</button>
+      <p>React DOM Updates: {Updates}</p>
+    </div>
+  );
 }
 
-let root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(<App/>);
 
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>)
